055658a99ebb7b9d5bbf598905a6d41f702ca869
mashak000 <mashakgood@gmail.com>
2024-07-26 09:05:01 +0300

    updated info

---
+++ src/app/data.js
@@ -0,0 +1,74 @@
+export const data = [
+  {
+    id: 1,
+    name: "Букет из пионов с эустомой",
+    amount: 25,
+    price: 14500,
+    imagelink:
+      "https://res.cloudinary.com/dhwfr8jrw/image/upload/v1721924936/photo_2024-07-25_19.27.12_bs6lcb.jpg",
+  },
+  {
+    id: 2,
+    name: "Букет пионовидных роз мисти",
+    amount: 39,
+    price: 16000,
+    imagelink:
+      "https://res.cloudinary.com/dhwfr8jrw/image/upload/v1721924939/photo_2024-07-25_19.27.09_xmsfts.jpg",
+  },
+  {
+    id: 3,
+    name: "Букет пионовидных роз Бомбастик",
+    amount: 39,
+    price: 16000,
+    imagelink:
+      "https://res.cloudinary.com/dhwfr8jrw/image/upload/v1721924938/photo_2024-07-25_19.27.05_gv1l1t.jpg",
+  },
+  {
+    id: 4,
+    name: "Букет голландских гортензий",
+    amount: 9,
+    price: 8000,
+    imagelink:
+      "https://res.cloudinary.com/dhwfr8jrw/image/upload/v1721924937/photo_2024-07-25_19.27.01_oxxczg.jpg",
+  },
+  {
+    id: 5,
+    name: "Букет голландских гортензий",
+    amount: 19,
+    price: 17000,
+    imagelink:
+      "https://res.cloudinary.com/dhwfr8jrw/image/upload/v1721924938/photo_2024-07-25_19.26.57_ptmywf.jpg",
+  },
+  {
+    id: 6,
+    name: "Букет белых роз",
+    amount: 101,
+    price: 15000,
+    imagelink:
+      "https://res.cloudinary.com/dhwfr8jrw/image/upload/v1721924938/photo_2024-07-25_19.26.49_cryjnd.jpg",
+  },
+  {
+    id: 7,
+    name: "Букет красных роз",
+    amount: 51,
+    price: 7500,
+    imagelink:
+      "https://res.cloudinary.com/dhwfr8jrw/image/upload/v1721924940/photo_2024-07-25_19.26.40_ladlqb.jpg",
+  },
+  {
+    id: 8,
+    name: "Букет из пионов",
+    amount: 15,
+    price: 7500,
+    imagelink:
+      "https://res.cloudinary.com/dhwfr8jrw/image/upload/v1721924936/photo_2024-07-25_19.26.36_uzxe6b.jpg",
+  },
+  {
+    id: 9,
+    name: "Букет из гортензией с эустомой",
+    amount: 25,
+    price: 16500,
+    imagelink:
+      "https://res.cloudinary.com/dhwfr8jrw/image/upload/v1721924942/photo_2024-07-25_19.26.29_xtlfyg.jpg",
+  },
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,36 +2,12 @@
 
 import { useContext } from "react";
 import { webAppContext } from "./context/context";
+import { data } from "./data";
 import FlowerCard from "@/components/ui/FlowerCard";
 import Carusel from "@/components/ui/Carusel";
 
 export default function Home() {
   const app = useContext(webAppContext).appRef.current;
-
-  const flowers = [
-    {
-      id: 1111,
-      name: "Romashki",
-      price: 29000,
-      imagelink:
-        "https://dostavka-tsvety.ru/wp-content/uploads/2019/12/13b832a2f9fcffd057117c09dff04f8d.jpeg",
-    },
-    {
-      id: 2222,
-      name: "Roses",
-      price: 3000,
-      imagelink:
-        "https://m.media-amazon.com/images/I/61QFtjVVy5L._AC_SL1024_.jpg",
-    },
-    {
-      id: 3333,
-      name: "Nesabudki",
-      price: 7000,
-      imagelink:
-        "https://сады-эдема.рф/images/cms/data/Zvetochki/Prochie_mnogoletnie_rasteniya/Prochie_mnogoletnie_2021/Prochie_mnogoletnie/06555_nezabudka_002.jpg",
-    },
-  ];
-  
 
   return (
     <>
@@ -44,8 +20,8 @@ export default function Home() {
           <div className="font-medium text-sm text-center">
             I&apos;m Mini App for Telegram
           </div>
-          <Carusel items={flowers}/>
-          </div>
+          <Carusel items={data} />
+        </div>
       ) : (
         "loading"
       )}
